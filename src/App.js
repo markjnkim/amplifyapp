5f6f4d5f6aadeb7dcb556e2f1ed14a8ee1dc5fbe
markjnkim <markjnkim@gmail.com>
2020-08-19 19:57:52 -0700

    revert to basic auth

--- src/App.js
+++ src/App.js
@@ -2,26 +2,16 @@ import React from 'react';
 import Amplify from 'aws-amplify';
 import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import awsconfig from './aws-exports';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-      <body>
-        <AmplifyAuthenticator>
-          <div>
-            Amplify App
-            <AmplifySignOut />
-          </div>
-        </AmplifyAuthenticator>
-      </body>
+Amplify.configure(awsconfig);
+
+const App = () => (
+  <AmplifyAuthenticator>
+    <div>
+      My App
+      <AmplifySignOut />
     </div>
-  );
-}
+  </AmplifyAuthenticator>
+);
 
 export default App;
